dd7f2da4d5e005b4d3d1242a3ecb9be21938597b
varslax <mykyta.tkachov@pm.bet>
2021-02-20 22:16:15 +0200

    feat: setup admin panel

---
+++ src copy/components/EnhancedTableToolbar/index.jsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { lighten, makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+// import TextField from '@material-ui/core/TextField';
+// import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddIcon from '@material-ui/icons/Add';
+// import SearchIcon from '@material-ui/icons/Search';
+
+const useToolbarStyles = makeStyles(theme => ({
+  root: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(1),
+  },
+  highlight:
+    theme.palette.type === 'light'
+      ? {
+          color: theme.palette.secondary.main,
+          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+        }
+      : {
+          color: theme.palette.text.primary,
+          backgroundColor: theme.palette.secondary.dark,
+        },
+  title: {
+    flex: '1 1 100%',
+  },
+  search: {
+    width: 300,
+  },
+  icon: {
+    height: 24,
+  },
+}));
+
+const EnhancedTableToolbar = props => {
+  const classes = useToolbarStyles();
+  const {
+    name,
+    numSelected,
+    handleAdd,
+    handleDelete,
+    // searchValue,
+    // setSearchValue,
+  } = props;
+
+  return (
+    <Toolbar
+      className={clsx(classes.root, {
+        [classes.highlight]: numSelected > 0,
+      })}
+    >
+      {numSelected > 0 ? (
+        <Typography
+          className={classes.title}
+          color="inherit"
+          variant="subtitle1"
+          component="div"
+        >
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Typography
+          className={classes.title}
+          variant="h6"
+          id="tableTitle"
+          component="div"
+        >
+          {name.slice(0, 1).toUpperCase() + name.slice(1)}
+        </Typography>
+      )}
+      {/* <TextField
+        label="Search"
+        id="search"
+        className={classes.search}
+        size="small"
+        value={searchValue}
+        onChange={setSearchValue}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+            <SearchIcon />
+            </InputAdornment>
+            ),
+          }}
+          variant="outlined"
+        /> */}
+      {numSelected > 0 ? (
+        <Tooltip title="Delete">
+          <IconButton aria-label="delete" onClick={handleDelete}>
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <Tooltip title="Add">
+          <IconButton aria-label="add" onClick={handleAdd}>
+            <AddIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+    </Toolbar>
+  );
+};
+
+EnhancedTableToolbar.propTypes = {
+  name: PropTypes.string.isRequired,
+  numSelected: PropTypes.number.isRequired,
+  handleAdd: PropTypes.func.isRequired,
+  handleDelete: PropTypes.func.isRequired,
+};
+
+export default EnhancedTableToolbar;
